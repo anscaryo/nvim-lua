f988e46db75dcb5c163e28e92c3c261de477c898
anscaryo <anscaryo@gmail.com>
2024-03-31 15:25:32 +0200

    Instalo el plugin plenary necesario para telescope y ya funciona

--- lua/plugins-setup.lua
+++ lua/plugins-setup.lua
@@ -42,6 +42,7 @@ return require('packer').startup(function(use)
 	use "nvim-treesitter/nvim-treesitter"
   use "nvim-tree/nvim-web-devicons"
   use "nvim-lua/plenary.nvim"
+
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.6',
   }	
